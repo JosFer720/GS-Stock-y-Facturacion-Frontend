767a25b5bff5c22657a35b753e876e253ad7b269
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2025-04-23 19:51:42 -0600

    Migracion base de datos

---
+++ GS-Stock/db-init/05-add-email-to-users.sql
@@ -0,0 +1,12 @@
+-- Agregar la columna email
+ALTER TABLE Usuarios ADD COLUMN Email VARCHAR(100);
+
+-- Actualizar los registros existentes con correos electrónicos
+UPDATE Usuarios SET Email = 'carlos.hernandez@example.com' WHERE Id = 1;
+UPDATE Usuarios SET Email = 'ana.ruiz@example.com' WHERE Id = 2;
+UPDATE Usuarios SET Email = 'luis.perez@example.com' WHERE Id = 3;
+UPDATE Usuarios SET Email = 'marta.gomez@example.com' WHERE Id = 4;
+
+-- Agregar las restricciones NOT NULL y UNIQUE
+ALTER TABLE Usuarios ALTER COLUMN Email SET NOT NULL;
+ALTER TABLE Usuarios ADD CONSTRAINT email_unique UNIQUE (Email);
